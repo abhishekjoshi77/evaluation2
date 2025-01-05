7831580da70f8b6e6484135e7bffe47528e5b87b
abhishekjoshi77 <94050150+abhishekjoshi77@users.noreply.github.com>
2025-01-05 16:20:08 +0530

    Create question4.js

---
+++ question4.js
@@ -0,0 +1,30 @@
+const files = [
+ "document1.pdf",
+ "document2.pdf",
+ "image1.png",
+ "image2.png",
+ "text1.txt",
+ "text2.txt",
+ "photo1.jpg",
+ "photo2.jpg",
+ "program1.exe",
+ "program2.exe",
+ "data1.csv",
+ "data2.csv",
+ "report1.pdf",
+ "report2.pdf",
+ "image3.png",
+ "text3.txt",
+ "photo3.jpg",
+ "document3.pdf"
+];
+
+
+{
+  pdf: ["document1.pdf", "document2.pdf", "report1.pdf", "report2.pdf", "document3.pdf"],
+  png: ["image1.png", "image2.png", "image3.png"],
+  txt: ["text1.txt", "text2.txt", "text3.txt"],
+  jpg: ["photo1.jpg", "photo2.jpg", "photo3.jpg"],
+  exe: ["program1.exe", "program2.exe"],
+  csv: ["data1.csv", "data2.csv"]
+}
