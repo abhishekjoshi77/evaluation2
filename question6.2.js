d9cef391f61d3ba4e5e86f00676acfa79f0bf8e5
abhishekjoshi77 <94050150+abhishekjoshi77@users.noreply.github.com>
2025-01-05 17:01:29 +0530

    Update question6.2.js

--- question6.2.js
+++ question6.2.js
@@ -18,3 +18,13 @@ const voters = [
 ];
 
 console.log(analyzeVoterData(voters));
+
+
+{
+  numYoungVotes: /* Number of young voters (aged 18-25) who voted */,
+  numYoungPeople: /* Number of young people (aged 18-25) */,
+  numMidVotesPeople: /* Number of middle-aged voters (aged 26-35) who voted */,
+  numMidsPeople: /* Number of middle-aged people (aged 26-35) */,
+  numOldVotesPeople: /* Number of older voters (aged 36 and above) who voted */,
+  numOldsPeople: /* Number of older people (aged 36 and above) */
+}
