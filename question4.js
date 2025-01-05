5378c206cd5d7e8855b8f4ad91552b06022c52d9
abhishekjoshi77 <94050150+abhishekjoshi77@users.noreply.github.com>
2025-01-05 16:27:04 +0530

    Update question4.js

--- question4.js
+++ question4.js
@@ -1,30 +1,38 @@
+function segregateFiles(fileNames) {
+  return fileNames.reduce((acc, fileName) => {
+    const extension = fileName.split('.').pop(); 
+    if (!acc[extension]) {
+      acc[extension] = []; 
+    }
+    acc[extension].push(fileName); 
+    return acc;
+  }, {}); 
+}
+
+// Given filenames
 const files = [
- "document1.pdf",
- "document2.pdf",
- "image1.png",
- "image2.png",
- "text1.txt",
- "text2.txt",
- "photo1.jpg",
- "photo2.jpg",
- "program1.exe",
- "program2.exe",
- "data1.csv",
- "data2.csv",
- "report1.pdf",
- "report2.pdf",
- "image3.png",
- "text3.txt",
- "photo3.jpg",
- "document3.pdf"
+  "document1.pdf",
+  "document2.pdf",
+  "image1.png",
+  "image2.png",
+  "text1.txt",
+  "text2.txt",
+  "photo1.jpg",
+  "photo2.jpg",
+  "program1.exe",
+  "program2.exe",
+  "data1.csv",
+  "data2.csv",
+  "report1.pdf",
+  "report2.pdf",
+  "image3.png",
+  "text3.txt",
+  "photo3.jpg",
+  "document3.pdf"
 ];
 
+// Function call
+const result = segregateFiles(files);
+console.log(result);
 
-{
-  pdf: ["document1.pdf", "document2.pdf", "report1.pdf", "report2.pdf", "document3.pdf"],
-  png: ["image1.png", "image2.png", "image3.png"],
-  txt: ["text1.txt", "text2.txt", "text3.txt"],
-  jpg: ["photo1.jpg", "photo2.jpg", "photo3.jpg"],
-  exe: ["program1.exe", "program2.exe"],
-  csv: ["data1.csv", "data2.csv"]
-}
+
