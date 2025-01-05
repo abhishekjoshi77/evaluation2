b9d9d4de436f64b6c644ef3cdbdde5b0cb15f50f
abhishekjoshi77 <94050150+abhishekjoshi77@users.noreply.github.com>
2025-01-05 15:28:06 +0530

    Update question1.js

--- question1.js
+++ question1.js
@@ -1,1 +1,10 @@
+// Rearrange Fruits
 
+function rearrangeFruits(fruits) {
+    const remove = fruits.splice(-4);
+    fruits.splice(0, 0,... remove);
+    return fruits;
+}
+const fruits = ["Apple", "Banana", "Orange", "Mango", "Pineapple", "Grapes", "Strawberry", "Watermelon", "Peach", "Kiwi"];
+
+console.log(rearrangeFruits(fruits));
