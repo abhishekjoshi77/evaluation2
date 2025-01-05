f156a0cc82c1a361599462eb0aa8457a1e11565d
abhishekjoshi77 <94050150+abhishekjoshi77@users.noreply.github.com>
2025-01-05 16:30:09 +0530

    Create question5.js

---
+++ question5.js
@@ -0,0 +1,9 @@
+const employees = [
+ { name: "Alice", age: 30, salary: 50000, department: "HR" },
+ { name: "Bob", age: 35, salary: 60000, department: "Finance" },
+ { name: "Charlie", age: 28, salary: 55000, department: "HR" },
+ { name: "David", age: 40, salary: 70000, department: "IT" },
+ { name: "Eve", age: 32, salary: 65000, department: "Finance" },
+];
+
+console.log(groupByDepartment(employees));
